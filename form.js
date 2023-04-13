1ef94f9cecf88b43455b5f8d5ea2562bea26a761
Louis-OFP <louis170304@gmail.com>
2023-04-13 11:48:12 +0200

    update section 4/5 + js

---
+++ form.js
@@ -0,0 +1,35 @@
+//let name1 = document.getElementById(Prénom)
+//let name2 = document.getElementById(Nom)
+//let message = document.getElementById(texte)
+//let eMail = document.getElementById(Mail)
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Votre message a été envoyé avec succès!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oups! Votre adresse email n'est pas valide, votre message n'as pas pu être transmis"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oups! Votre adresse email n'est pas valide, votre message n'as pas pu être transmis"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
